5dc7cdfbd6436f563f89cd25b2508e137cc7d2db
ngan sat thu <thaivokimngan7998@gmail.com>
2021-06-07 20:51:48 +0700

    add crawl demo

--- crawl-demo.py
+++ crawl-demo.py
@@ -7,7 +7,7 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 soupFound = soup.find("div", {"id": "js-read__content"})
 
-deleteTags = ['br', 'a', 'small', 'script', 'div']
+deleteTags = ['br', 'a', 'small', 'script', 'div', "html"]
 for t in deleteTags:
     for e in soupFound.findAll(t):
         e.extract()
